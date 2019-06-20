d29f528181d2dc09bc617c145cfd1f952df3dd23
evgar <eugene.garichev@gmail.com>
2019-06-20 10:15:29 +0300

    fix

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -2,4 +2,4 @@ import { GITHUB_USERS } from './endpoints'
 
 export const getUsersInAmount = number => `${GITHUB_USERS}?per_page=${number}`;
 
-export const getNewUsers = number => `${GITHUB_USERgit addS}?since=${number}`;
+export const getNewUsers = number => `${GITHUB_USERS}?since=${number}`;
